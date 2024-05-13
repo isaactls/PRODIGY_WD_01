28ce5f39e049d0a0f3ed1eb9dab6d7d360286eee
isaactls <isacktolesa@gmail.com>
2024-05-13 18:42:05 +0300

    ok

--- landing_page/src/index.js
+++ landing_page/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/PRODIGY_WD_01/landing_page">
+  <BrowserRouter basename="/PRODIGY_WD_01">
     <App />
   </BrowserRouter>
 );
