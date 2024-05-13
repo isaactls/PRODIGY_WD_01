8143c102a6bdc855791ac5bc3dc328de61dde970
isaactls <isacktolesa@gmail.com>
2024-05-13 14:32:43 +0300

    hello

--- landing_page/src/components/Nav.jsx
+++ landing_page/src/components/Nav.jsx
@@ -7,16 +7,16 @@ function Nav() {
     <nav className="nav-container">
       <ul className="nav-items">
         <li>
-          <NavLink to="/PRODIGY_WD_01/landing_page/"> Home</NavLink>
+          <NavLink to="/PRODIGY_WD_01/"> Home</NavLink>
         </li>
         <li>
-          <NavLink to="/PRODIGY_WD_01/landing_page/About">About</NavLink>
+          <NavLink to="/PRODIGY_WD_01/About">About</NavLink>
         </li>
         <li>
-          <NavLink to="/PRODIGY_WD_01/landing_page/services">Services</NavLink>
+          <NavLink to="/PRODIGY_WD_01/services">Services</NavLink>
         </li>
         <li>
-          <NavLink to="/PRODIGY_WD_01/landing_page/contact">contact</NavLink>
+          <NavLink to="/PRODIGY_WD_01/contact">contact</NavLink>
         </li>
       </ul>
     </nav>
